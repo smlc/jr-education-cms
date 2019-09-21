0c55d62f6183407b2d666994d430cbc6babac086
smlc <siesa.marlon@hotmail.fr>
2019-09-21 13:06:44 +0200

    everthing ok for create page from markdonw

--- gatsby-node.js
+++ gatsby-node.js
@@ -28,11 +28,11 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `)
-
+  console.log(JSON.stringify(result, null, 4))
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
-      component: path.resolve(`./src/templates/blog-post.js`),
+      component: path.resolve(`./src/templates/cour-post.js`),
       context: {
         // Data passed to context is available
         // in page queries as GraphQL variables.
---
+++ src/templates/cour-post.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default ({ data }) => {
+  return (
+    <Layout>
+      <div>
+        <h1>Cour post</h1>
+      </div>
+    </Layout>
+  )
+}
