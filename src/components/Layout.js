d300b582e59171efcdb3699ca719b594e3cac4c7
smlc <siesa.marlon@hotmail.fr>
2020-03-05 22:46:14 +0100

    home done

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,30 +2,25 @@ import React from "react"
 import { Link } from "gatsby"
 
 const Layout = ({ children }) => (
-  <div class="columns is-fullheight">
-    <div
-      style={{ maxWidth: 400 }}
-      class="column is-two-fifths has-background-dark is-sidebar-menu"
-    >
-      <aside
-        style={{ margin: `8rem 1rem 10rem`, maxWidth: 650, padding: `0 1rem` }}
-        class="menu"
-      >
-        <p class="menu-label">Menu</p>
-        <ul class="menu-list">
-          <li>
-            <Link to="/cours">Cour et exercices</Link>
-          </li>
-          <li>
-            <a href="#top">Numérique et algorithmique</a>
-          </li>
-          <li>
-            <a href="#top">Brevet et Bac</a>
-          </li>
-        </ul>
-      </aside>
+
+    <div class="flex items-stretch h-screen">
+      <div class="bg-gray-200 w-2/12 border-r-2">
+        <aside>
+          <p class="text-3xl pl-8 pt-12 text-gray-800">Menu</p>
+          <ul class="menu-list py-32 pl-8">
+            <li class="py-8 text-gray-600 hover:text-gray-800">
+              <Link to="/cours">Cour et exercices</Link>
+            </li>
+            <li class="py-8 text-gray-600 hover:text-gray-800">
+              <a href="#top">Numérique et algorithmique</a>
+            </li>
+            <li class="py-8 text-gray-600 hover:text-gray-800">
+              <a href="#top">Club informatique</a>
+            </li>
+          </ul>
+        </aside>
+      </div>
+      <div class="w-5/6 bg-white">{children}</div>
     </div>
-    <div class="column">{children}</div>
-  </div>
 )
-export default Layout;
+export default Layout
